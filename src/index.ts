306a9ffa0576fc24543a8407e90a504fb66e81e2
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-11-21 18:11:06 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, {
+    type Application,
+    type Request,
+    type Response,
+} from "express";
+import cors from "cors";
+import "dotenv/config";
+// import router from "./routes";
+
+const app: Application = express();
+const PORT: string | number = process.env.PORT ?? 9090;
+
+const corOptions = {
+    origin: "http://localhost:3000",
+    methods: "GET,PATCH,POST,DELETE",
+};
+
+app.use(cors(corOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Hello from API Services!" });
+});
+
+// app.use("/v1", router);
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Server is listening PORT: ${PORT}`);
+    });
+} catch (error: any) {
+    console.log(`Error occurred: ${error.message}`);
+}
